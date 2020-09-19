f907f3826afd7d5da531eafb0f0422f9b7d8d6ac
Mahsa Farahani <38652406+mahsafrhni@users.noreply.github.com>
2020-09-19 11:25:17 +0430

    video 3 & 4

--- functions.js
+++ functions.js
@@ -1,3 +1,24 @@
 function changeText() {
     document.getElementById("p1").innerHTML = "Visit <a href='https://github.com/mahsafrhni'>My Git</a>"
 }
+
+function getNumbers() {
+    var n1 = parseInt(prompt("enter first number:"));
+    var n2 = eval(prompt("enter second number:"));
+    var operate = prompt("enter operate:");
+    var result = calc(n1, n2, operate);
+    alert(result);
+}
+
+function calc(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+        case "*":
+            return num1 * num2;
+        case "/":
+            return num1 / num2;
+    }
+}
